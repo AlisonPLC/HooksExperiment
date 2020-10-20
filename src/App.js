de2c3fd146e0ec15c642a32785264317eecab6e7
AlisonPLC <alisonpietro@gmail.com>
2020-10-20 00:47:30 -0300

    initial advanced commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,24 @@
 import React, {useState, useEffect} from 'react';
 
 export default function App(){
-  const [repositories, setRepositories] = useState([]);
+  const [location, setLocation] = useState({});
 
-  useEffect(async()=> {
- const response = await fetch("https://api.github.com/users/AlisonPLC/repos");
- const data = await response.json();
+  useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(handlePositionRecieved)
 
- setRepositories(data);
+    return () => navigator.geolocation.clearWatch(watchId)
   },[]);
 
-  useEffect(()=>{
-    const filtered = repositories.filter(repo => repo.favorite);
-
-    document.title = `Vc tem  ${filtered.length} Favoritos`;
-
-  },[repositories]);
-
- function HandleFavorite(id){
-  const newRepositories = repositories.map(repo =>{
-    return repo.id == id ? {...repo,favorite: !repo.favorite } : repo
-  })
-  setRepositories(newRepositories);
- }
- 
+  function handlePositionRecieved({coords}){
+    
+    const {latitude,longitude} = coords;
+    setLocation({latitude,longitude});
+  }
 
   return (
-    <div style={{backgroundColor: "lightblue"}}>
-      <h2>Repositorios gerais</h2>
-    <ul>
-      {repositories.map(repo => (
-      <li key={repo.id}>{repo.name} {repo.favorite && <span>(Favorito)</span>}
-        <button onClick={() => HandleFavorite(repo.id)}>Favoritar</button>
-      </li>
-      ))}
-    </ul>
-    </div>
+    <>{/* isso aqui é um fragment, basicamente uma div sem ser div */}
+      Latitude: {location.latitude} <br/>
+      Longitude: {location.longitude}<br/>
+    </>
   );
 }
